8172c0fd80ce22952e746e4510af66ef03493eea
Jan Biehl <jpbiehl@googlemail.com>
2022-01-02 20:52:00 +0100

    program updates

--- PlcSimAdvancedGateway/Program.cs
+++ PlcSimAdvancedGateway/Program.cs
@@ -1,39 +1,79 @@
 ﻿using System.Runtime.InteropServices;
 using CommandLine;
 using Grpc.Core;
+using Grpc.Reflection;
+using Grpc.Reflection.V1Alpha;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PlcSimAdvanced.Protos;
 using PlcSimAdvancedGateway;
+using PlcSimAdvancedGateway.Services;
+using PlcSimService = PlcSimAdvancedGateway.Services.PlcSimService;
+
+// set up the dependency injection
+await using var serviceProvider = new ServiceCollection()
+    .AddLogging((builder) =>
+    {
+        builder
+            .AddFilter("Microsoft", LogLevel.Warning)
+            .AddFilter("System", LogLevel.Warning)
+            .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+            .AddConsole();
+    })
+    .AddSingleton<PlcInstanceManager>()
+    .AddSingleton<PlcSimService>()
+    .AddSingleton<ControllerService>()
+    .BuildServiceProvider();
+
+// get the logger
+ILogger logger = (serviceProvider.GetService<ILoggerFactory>() ?? throw new InvalidOperationException("There is no logger"))
+    .CreateLogger<Program>();
 
 // ensure the correct os, since PlcSimAdvanced from Siemens got a windows dependency,
 // we will fail when not running on windows
 if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 {
-    Console.WriteLine("This application will only run on windows");
+#if !DEBUG_MAC
+    logger.LogError("this application will only run on windows");
     return (int) ExitCode.WrongOs;
+#else
+    logger.LogWarning("application is running on a unsupported os, only basic testing is enabled");
+#endif
 }
 
 // parse the command line args
 Parser.Default.ParseArguments<CommandLineOptions>(args)
     .WithParsed(options => CommandLineOptions.Options = options);
 
-Console.WriteLine("setting up the grpc server...");
+logger.LogInformation("setting up the grpc server");
+
+var reflectionService = new ReflectionServiceImpl(
+    PlcSimAdvanced.Protos.PlcSimService.Descriptor,
+    VirtualControllerService.Descriptor, 
+    ServerReflection.Descriptor);
 
 // setup the grpc server
 Server server = new()
 {
-    Services = {PlcSimAdvanced.Protos.PlcSimAdvancedService.BindService(new PlcSimAdvancedService())},
+    Services = 
+    {
+        ServerReflection.BindService(reflectionService),
+        PlcSimService.BindService(serviceProvider.GetService<PlcSimService>()),
+        VirtualControllerService.BindService(serviceProvider.GetService<ControllerService>()),
+    },
     Ports = {new("localhost", CommandLineOptions.Options.Port, ServerCredentials.Insecure)}
 };
 
 // start the grpc server
 server.Start();
 
-Console.WriteLine($"... grpc server started. listening on port: '{CommandLineOptions.Options.Port}'");
+logger.LogInformation($"grpc server started, it is listening on port: '{CommandLineOptions.Options.Port}'");
 
-Console.WriteLine("Press any key to stop the server...");
+logger.LogInformation("Press any key to stop the application...");
 Console.ReadKey();
 
-Console.WriteLine("application is shutting down...");
+logger.LogInformation("application is shutting down...");
 await server.ShutdownAsync();
-Console.WriteLine("application stopped");
+logger.LogInformation("application stopped");
 
 return 0;
